9c3e291e040430740ff6df83eedb5aa07a0f5cbe
Nelamin2 <nidalelamin@gmail.com>
2023-09-08 10:34:13 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
   * main - Prints a hexadecimal string
   *
@@ -7,18 +6,18 @@
   */
 int main(void)
 {
-	char c;
+char c;
 
-	for (c = '0'; c <= '9'; c++)
+for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
 	}
 
-	for (c = 'a'; c <= 'f'; c++)
+for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar(c);
 	}
 
-	putchar('\n');
+putchar('\n');
 
-	return (0);
+return (0);
