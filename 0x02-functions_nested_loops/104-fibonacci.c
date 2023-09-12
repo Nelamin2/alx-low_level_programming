223342c46bb7fbf29e8227a26e95c542c5ba5cbe
Nelamin2 <nidalelamin@gmail.com>
2023-09-12 14:02:16 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,7 +2,7 @@
 #define LARGEST 10000000000
 /**
  * main - main function
- * Description: Find and print the first 98 fibonacci numbers starting with 1 and 2.
+ * Description: Find and print the first 98 fib numbs starting with 1 and 2.
  * Return: 0
  * Numbers should be coma and space separated.
  */
