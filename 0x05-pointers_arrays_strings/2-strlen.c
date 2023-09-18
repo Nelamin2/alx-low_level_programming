3c545a3817401fc5e564423aa054196268257fa1
Nelamin2 <nidalelamin@gmail.com>
2023-09-18 05:12:55 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *p)
+{
+	int length = 0;
+
+	while (*p != '\0')
+	{
+		length++;
+		p++;
+	}
+
+	return (longi);
+}
+
