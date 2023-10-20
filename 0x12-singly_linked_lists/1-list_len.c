cc4b40bee95d3f9a459db36a7e899a9b8f17a8db
Nelamin2 <nidalelamin@gmail.com>
2023-10-20 16:58:53 -0700

    task1-1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,7 +4,7 @@
 #include "lists.h"
 
 /**
- *i list_len - returns the number of elements in a linked list
+ *list_len - returns the number of elements in a linked list
  * @h: pointer to the list_t list
  *
  * Return: number of elements in h
