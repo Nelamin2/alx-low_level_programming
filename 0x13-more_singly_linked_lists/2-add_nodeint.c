fe657d097c0a0f5d3e5ba4794dcd8c7505992279
Nelamin2 <nidalelamin@gmail.com>
2023-10-23 15:56:42 -0700

    task2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,11 +12,13 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {listint_t *ptr;
 ptr = malloc(sizeof(listint_t));
-ptr->n = n;
-ptr->next = NULL;
-if (!ptr)
+if (ptr == NULL)
 return (NULL);
-ptr->next = head;
-head = ptr;
+if (*head == NULL)
+ptr->next == NULL,
+else
+ptr->next = *head;
+ptr->n = n;
+*head = ptr;
 return (head);
 }
