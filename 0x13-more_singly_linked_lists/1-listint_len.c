07f1fd8ff6321cc5d1b7b10f4d99b0f3866c8337
Nelamin2 <nidalelamin@gmail.com>
2023-10-23 14:21:20 -0700

    task1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* listint_len -  returns  the number of elementd in a linked list
+* @h: a pointer to the first element of the linked list
+* return: the number of the nodes
+*/
+size_t listint_len(const listint_t *h)
+{
+size_t count;
+count = 0;
+while (h)
+{ count++;
+h = h->next;
+}
+return (count);
+}
