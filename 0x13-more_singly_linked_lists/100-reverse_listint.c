d6308d5f889fde4587567faa88a4e3d794ab7966
Nelamin2 <nidalelamin@gmail.com>
2023-10-24 16:56:43 -0700

    task11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -13,11 +13,11 @@
 listint_t *reverse_listint(listint_t **head)
 {
 listint_t *past = NULL;
-listint_t *next = NULL;
+listint_t *lead = NULL;
 while (*head)
 {
 lead = (*head)->next;
-(*head)->next = prev;
+(*head)->next = lead;
 past = *head;
 *head = lead;
 }
