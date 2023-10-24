44994b9626eefbc4fcedb2a3c3ca877b701ab72a
Nelamin2 <nidalelamin@gmail.com>
2023-10-24 10:41:26 -0700

    task4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint - free the linked list
+ *@head: points to the fisrt element in the linked list
+ */
+
+void free_listint(listint_t *head)
+{ listint_t *ptr;
+while (ptr != NULL)
+{Ptr = Ptr->link;
+free(head);
+head = ptr;
+}
+}
+
+
