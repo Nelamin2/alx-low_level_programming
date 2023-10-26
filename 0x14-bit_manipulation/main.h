15eaaa5b31e5a0aa421e5cd800f067756af760c4
Nelamin2 <nidalelamin@gmail.com>
2023-10-26 13:41:45 -0700

    task2

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 
 int _putchar(char c);
+void check(unsigned int);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
