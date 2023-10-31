9879573fcd9e06498aad7ec00ae3babe145b0125
Nelamin2 <nidalelamin@gmail.com>
2023-10-30 19:32:04 -0700

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,36 +5,35 @@
 #include<fcntl.h>
 #include<stdlib.h>
 
-/*
- * read_textfile- reads a text file and prints it to the POSIX standard output.
+/**
+ * read_textfile - reads a text file and prints
  * @filename: pointer to file.
  * @letters: the number of letters it should read and print
  * Return: actual number of letters it could read and print or
  * 0 if the file can not be opened or read
- **/
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *buffer;
 int discriptor;
 ssize_t numbers_r, numbers_w;
-if(filename == NULL)
+if (filename == NULL)
 return (0);
 discriptor  = open(filename, O_RDWR);
-if(discriptor == -1)
+if (discriptor == -1)
 return (0);
-buffer = malloc (sizeof(char) * letters);
-if(buffer == NULL)
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
 close(discriptor);
 return (0);
 numbers_r = read(discriptor, buffer, letters);
 close(discriptor);
-if(numbers_r == -1)
+if (numbers_r == -1)
 free(buffer);
 numbers_w = write(STDOUT_FILENO, buffer, numbers_r);
-if(numbers_r != numbers_w)
-return(0);
-else
+if (numbers_r != numbers_w)
+return (0);
 return (numbers_w);
 }
 
