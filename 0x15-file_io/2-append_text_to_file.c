c596d4a157697b6a822c515ae4ba9b1d139ab68d
Nelamin2 <nidalelamin@gmail.com>
2023-11-01 06:44:38 -0700

    task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,14 +1,14 @@
 #include "main.h"
 #include<unistd.h>
 #include<sys/types.h>
-#include<sys.stats.h>
+#include<sys/stat.h>
 #include<fcntl.h>
 #include<stdlib.h>
 
 /**
  * append_text_to_file - function to append
  * @filename: a pointer to afile
- * @text_content: pointer to a file
+ * @text_content: string to be added
  * Return: 1 on success and -1 on failure
  * filename is NULL return -1
  */
@@ -19,16 +19,16 @@ int rw;
 int new;
 if (!filename)
 return (-1);
-d = open(filename, O_RDONLY | O_APPEND);
-if (d == -1)
+discriptor = open(filename, O_RDWR | O_APPEND);
+if (discriptor == -1)
 return (0);
 if (text_content)
 {
-for (new = 0; text_content[new], new++)
-rw = write(d, text_content, new);
+for (new = 0; text_content[new]; new++)
+rw = write(discriptor, text_content, new);
 if (rw == -1)
 return (-1);
-close(d);
+close(discriptor);
 }
 return (1);
 }
