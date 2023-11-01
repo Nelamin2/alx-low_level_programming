d15c077f327116fd64a6e831bda85843f415226f
Nelamin2 <nidalelamin@gmail.com>
2023-11-01 03:41:52 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,3 +1,9 @@
+#include "main.h"
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<stdlib.h>
 
 /**
  * create_file - create a file and write on it
@@ -13,16 +19,17 @@ int rw;
 int new;
 if (!filename)
 return (-1);
-d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (d == -1)
+discriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (discriptor == -1)
 return (0);
 if (!text_content)
 return (-1);
 text_content = "";
-for (new = 0; text_content[new], new++)
-rw = write(d, text_content, new);
+for (new = 0; text_content[new]; new++)
+{rw = write(discriptor, text_content, new);
 if (rw == -1)
 return (-1);
 close(discriptor);
 }
 return (1);
+}
