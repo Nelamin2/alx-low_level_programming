a940d45cc0e101ee03efc2ab04ed460557ba19ea
Nelamin2 <nidalelamin@gmail.com>
2023-11-02 17:18:57 -0700

    task4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -10,22 +10,20 @@
  */
 void print_elf_header(Elf64_Ehdr *header)
 {
+int i;
 printf("  Magic:   ");
-for (int i = 0; i < EI_NIDENT; i++)
-{
-printf("%02x ", header->e_ident[i]);
-}
-printf("\n");
-printf("Class:%s\n", header->e_ident[EI_CLASS]
-== ELFCLASS64 ? "ELF64" : "ELF32");
-printf("Data:%s\n", header->e_ident[EI_DATA] ==
-ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-printf("Version:%d (current)\n", header->e_ident[EI_VERSION]);
-printf("OS/ABI:%d\n", header->e_ident[EI_OSABI]);
-printf("ABI Version:%d\n", header->e_ident[EI_ABIVERSION]);
-printf("Type:%d\n", header->e_type);
-printf("Entry point address:%#lx\n", (unsigned long) header->e_entry);
-}
+for (i = 0; i < EI_NIDENT; i++)
+ printf("%02x ", header->e_ident[i]);
+    }
+    printf("\n");
+    printf("  Class:                             %s\n", (header->e_ident[EI_CLASS] == ELFCLASS64) ? "ELF64" : "ELF32");
+    printf("  Data:                              %s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "2's complement, big endian");
+    printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+    printf("  OS/ABI:                            %d\n", header->e_ident[EI_OSABI]);
+    printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+    printf("  Type:                              %d\n", header->e_type);
+    printf("  Entry point address:               %#lx\n", (unsigned long)header->e_entry);
+}}
 /**
  * main - Displays the information contained in the ELF header of an ELF file.
  * @argc: Number of arguments.
@@ -33,7 +31,7 @@ printf("Entry point address:%#lx\n", (unsigned long) header->e_entry);
  * Return: 0 on success, 98 on failure.
  */
 int main(int argc, char *argv[])
-{
+{ 
 if (argc != 2)
 {
 fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
