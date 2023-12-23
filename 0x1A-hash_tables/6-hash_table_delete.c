81b50e53579ad0296caef2379f66a5b4707e096a
Nelamin2 <nidalelamin@gmail.com>
2023-12-22 16:20:11 -0800

    task6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,22 +8,24 @@
  * @ht: A pointer to a hash table.
  */
 void hash_table_delete(hash_table_t *ht)
-{
-hash_node_t *head;
+hash_table_t *head = ht;
+hash_node_t *node, *tmp;
 unsigned long int i;
 for (i = 0; i < ht->size; i++)
 {
 if (ht->array[i] != NULL)
 {
-head = ht->array[i]->next;
-free(ht->array[i]->key);
-free(ht->array[i]->value);
-free(ht->array[i]);
-ht->array[i] = head;
+node = ht->array[i];
+while (node != NULL)
+{
+tmp = node->next;
+free(node->key);
+free(node->value);
+free(node);
+node = tmp;
 }
 }
-free(ht->array);
-ht->array = NULL;
-ht->size = 0;
-free(ht);
+}
+free(head->array);
+free(head);
 }
